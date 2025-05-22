0e7e53dba432c7444b30f1e5e12bb9095bfc9351
Waheeb <waheebbasalem@gmail.com>
2025-05-22 05:13:57 +0300

    created a socket that handles one connection

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,10 +1,21 @@
 import requests
+from threading import Thread
+from src import server
+
 
 def test_connect_with_one_user():
+    server_thread = Thread(target=server.create_server, args=("127.0.0.1", 8080))
+    server_thread.start()
     response = requests.get("http://127.0.0.1:8080")
     assert response.status_code == 200
+
 
 def test_connect_with_multiple_users():
     for _ in range(5):
         response = requests.get("http://127.0.0.1:8080")
         assert response.status_code == 200
+
+
+if __name__ == "__main__":
+    test_connect_with_one_user()
+    # test_connect_with_multiple_users()
