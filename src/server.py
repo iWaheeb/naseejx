cebf4e5b86c09b311e8245602497ddf0dbfabb33
Waheeb <waheebbasalem@gmail.com>
2025-05-27 23:52:26 +0300

    handle multiple requests without closing the server (one request at a time)

--- src/server.py
+++ src/server.py
@@ -7,12 +7,13 @@ def create_server(ip:str, port: int):
     listen_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
     listen_sock.bind((ip, port))
     listen_sock.listen()
-    conn, addr = listen_sock.accept()
-    print(f"{addr} connected")
-    request = conn.recv(1024)
-    print(request)
-    CRLF = "\r\n"
-    response = f"HTTP/1.0 200 OK{CRLF+CRLF}"
-    conn.send(response.encode())
-    print("end")
-    listen_sock.close()
+    while True:
+        conn, addr = listen_sock.accept()
+        print(f"{addr} connected")
+        request = conn.recv(1024)
+        print(request)
+        CRLF = "\r\n"
+        response = f"HTTP/1.0 200 OK{CRLF+CRLF}"
+        conn.send(response.encode())
+        print("end")
+        conn.close()
