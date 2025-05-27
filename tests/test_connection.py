f597c7ecf8efef9f7b7815bd9460b99a2cdc9911
Waheeb <waheebbasalem@gmail.com>
2025-05-27 23:50:19 +0300

    improved debugging experience
    changed pytest config to work with vscode debugger
    
    added vscode config
    
    updated .gitignore

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -14,8 +14,3 @@ def test_connect_with_multiple_users():
     for _ in range(5):
         response = requests.get("http://127.0.0.1:8080")
         assert response.status_code == 200
-
-
-if __name__ == "__main__":
-    test_connect_with_one_user()
-    # test_connect_with_multiple_users()
